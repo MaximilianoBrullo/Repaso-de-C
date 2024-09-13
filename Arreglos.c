132c0b2d5845e3781728a2b0da0313b60d71405c
MaximilanoBrullo <maximilianobrullo4@gmail.com>
2024-09-13 01:55:39 -0300

    I don't know what to write about this file

---
+++ Arreglos.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*cuenta digitos, espacios en blanco y otros*/
+main(){
+    int c, i, nwhite, nother;
+    int ndigit[10];
+
+    nwhite = nother = 0;
+    for(i = 0; i < 10; ++i){
+        ndigit[i] = 0;
+    }
+    while ((c = getchar()) != EOF){
+        if(c> = '')
+    }
+    
+}
