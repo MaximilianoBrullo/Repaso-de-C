501d1a3c2d7e22739e2706467e2a97baab81df41
MaximilanoBrullo <maximilianobrullo4@gmail.com>
2024-09-13 09:35:48 -0300

    file Arreglos.c si added

--- Arreglos.c
+++ Arreglos.c
@@ -9,7 +9,19 @@ main(){
         ndigit[i] = 0;
     }
     while ((c = getchar()) != EOF){
-        if(c> = '')
+        if(c >= '0' && c <= '9'){
+            ++ndigit[c-'0'];
+        }else if(c == ' ' || c == '\n' || c == '\t'){
+            ++nwhite;
+        }else{
+            ++nother;
+        }
+        printf("digitos =");
+        for(i = 0; i < 10; ++i){
+            printf(" %d", ndigit[i]);
+            printf(", espacios blancos = %d, otros = %d\n",
+            nwhite, nother);
+        }
     }
     
 }
