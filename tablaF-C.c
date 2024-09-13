9cd07d5e781077896ce3b99df207b7a88bb95d3b
MaximilanoBrullo <maximilianobrullo4@gmail.com>
2024-09-13 01:11:59 -0300

    file tablaF-C.c is added

--- tablaF-C.c
+++ tablaF-C.c
@@ -9,11 +9,11 @@ main(){
 
     lower = -50;
     upper = 400;
-    step =10;
+    step = 10;
     
     fahr = lower;
     while(fahr <= upper){
-        celsius = 5*(fahr - 32) / 9;
+        celsius = 5 * (fahr - 32) / 9;
         printf("%d\t%d\n", fahr, celsius);
         fahr = fahr + step;
     }
