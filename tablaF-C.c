43ec0149712329e46ef64825efebbb9808604dd1
MaximilanoBrullo <maximilianobrullo4@gmail.com>
2024-09-13 00:51:39 -0300

    tablaF-C.c is added

---
+++ tablaF-C.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/*Imprimie una tabla de grados Fahrenheit y 
+su equivalente en Celsuis*/
+
+main(){
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = -50;
+    upper = 400;
+    step =10;
+    
+    fahr = lower;
+    while(fahr <= upper){
+        celsius = 5*(fahr - 32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
